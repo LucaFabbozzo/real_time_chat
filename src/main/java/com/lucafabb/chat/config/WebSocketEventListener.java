613caeaf25641a4b5e345c47c56b87a5e5783e8a
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-08-16 16:33:23 +0200

    finish the disconnect event listener

--- src/main/java/com/lucafabb/chat/config/WebSocketEventListener.java
+++ src/main/java/com/lucafabb/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.lucafabb.chat.config;
 
+import com.lucafabb.chat.chat.ChatMessage;
+import com.lucafabb.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,10 +15,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ) {
-        //TODO -- to be implemented
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
