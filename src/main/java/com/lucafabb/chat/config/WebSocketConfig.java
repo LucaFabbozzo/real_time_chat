6f6fa2cc620a58a2889d4aea67665c5de8a225d3
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-08-16 18:13:50 +0200

    add comments config classes

--- src/main/java/com/lucafabb/chat/config/WebSocketConfig.java
+++ src/main/java/com/lucafabb/chat/config/WebSocketConfig.java
@@ -1,3 +1,7 @@
+/*In sintesi, questo codice configura un'applicazione Spring per utilizzare i WebSocket e il protocollo STOMP per abilitare la comunicazione in tempo reale tra il client e il server. L'endpoint WebSocket è registrato su "/ws" e il message broker è configurato per utilizzare i prefissi "/app" per le richieste dei client e "/topic" per inoltrare i messaggi ai client.*/
+
+
+
 package com.lucafabb.chat.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -6,14 +10,19 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
+/*Queste annotazioni indicano a Spring che questa classe è una configurazione e che deve essere abilitata per gestire i WebSocket e il message broker.*/
 @Configuration
 @EnableWebSocketMessageBroker
+/*La classe WebSocketConfig implementa l'interfaccia WebSocketMessageBrokerConfigurer, che fornisce metodi per configurare i WebSocket e il message broker.*/
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    /*Questo metodo registra un'endpoint WebSocket "/ws" che i client possono utilizzare per stabilire una connessione WebSocket. Il metodo withSockJS() abilita la compatibilità con SockJS, una libreria JavaScript che fornisce un'astrazione per la comunicazione WebSocket e che è utile per gestire situazioni in cui WebSocket potrebbe non essere supportato.*/
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
     }
 
+    Q/*uesto metodo configura il message broker utilizzato per instradare i messaggi tra client e server. In questo caso, setApplicationDestinationPrefixes("/app") imposta il prefisso degli indirizzi destinazione delle richieste client che saranno instradate verso metodi di gestione nel server. enableSimpleBroker("/topic") abilita un message broker semplice che inoltrerà i messaggi ai client tramite i topic che iniziano con "/topic".*/
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
--- src/main/java/com/lucafabb/chat/config/WebSocketEventListener.java
+++ src/main/java/com/lucafabb/chat/config/WebSocketEventListener.java
@@ -1,3 +1,6 @@
+/*In sintesi, questo componente gestisce l'evento di disconnessione dei client WebSocket, registra il fatto che un utente è stato disconnesso e invia un messaggio a tutti i client per informarli della disconnessione.*/
+
+
 package com.lucafabb.chat.config;
 
 import com.lucafabb.chat.chat.ChatMessage;
@@ -10,13 +13,23 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+/*che indica che è un componente gestito da Spring*/
 @Component
+ /*è fornita dalla libreria Lombok e genera automaticamente un costruttore con tutti i campi finali richiesti.*/
 @RequiredArgsConstructor
+/*@Slf4j semplifica l'aggiunta di un logger alle tue classi, contribuendo a una migliore gestione dei messaggi di log per il debugging e il monitoraggio delle tue applicazioni.*/
 @Slf4j
 public class WebSocketEventListener {
 
+   /* L'oggetto SimpMessageSendingOperations è iniettato nel costruttore attraverso l'annotazione @RequiredArgsConstructor. Questo oggetto consente di inviare messaggi ai client tramite il broker dei messaggi.*/
     private final SimpMessageSendingOperations messageTemplate;
 
+   /* Metodo handleWebSocketDisconnectListener: Questo metodo è annotato con @EventListener e gestisce gli eventi di disconnessione dei client WebSocket. Prende in ingresso un oggetto SessionDisconnectEvent che rappresenta l'evento di disconnessione di una sessione WebSocket.*/
+   /*Viene utilizzato StompHeaderAccessor.wrap(event.getMessage()) per ottenere un oggetto StompHeaderAccessor che consente di accedere agli header e agli attributi del messaggio.*/
+    /*Viene recuperato il nome utente dall'attributo di sessione "username" usando headerAccessor.getSessionAttributes().get("username").
+    Se il nome utente non è nullo, viene registrato un messaggio di log che indica che l'utente è stato disconnesso.
+    Viene creato un oggetto ChatMessage che rappresenta un messaggio di chat di tipo "LEAVE" (indicante la disconnessione) e con il mittente (sender) impostato sul nome utente.
+    Infine, il messaggio viene inviato a tutti i client tramite il metodo convertAndSend dell'oggetto SimpMessageSendingOperations, utilizzando il topic "/topic/public" come destinazione.*/
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
